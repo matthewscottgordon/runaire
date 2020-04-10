38b34eb6d9218205664a38a8eaa9e641ce510123
Matthew Gordon <matthew.scott.gordon@gmail.com>
2020-04-10 12:01:34 -0400

    Add rocket web server to project.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket::{get, ignite, routes};
+
 use std::env;
 use std::error;
 
 mod database;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     const DB_FILE_PATH_ENV_VAR: &'static str = "RUNAIRE_DATABASE_FILE";
@@ -16,6 +25,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     println!("{}", db_file_path);
 
     database::init_database(&db_file_path)?;
+
+    ignite().mount("/", routes![index]).launch();
 
     Ok(())
 }
