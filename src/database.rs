f1648630aad8f4c5f65753ce42835b260d554973
Matthew Gordon <matthew.scott.gordon@gmail.com>
2020-04-10 12:24:55 -0400

    Set up rocket db connection pool

--- src/database.rs
+++ src/database.rs
@@ -1,4 +1,5 @@
-use rusqlite::{params, Connection, Result};
+use rusqlite::types::ToSql;
+use rusqlite::{Connection, Result};
 
 use std::fmt::Debug;
 use std::path::Path;
@@ -8,9 +9,9 @@ fn populate_db_with_test_data<P: AsRef<Path> + Debug>(db_file_path: P) -> Result
     let mut statement =
         conn.prepare("INSERT INTO todo_items (name, description, is_done) VALUES (?, ?, ?)")?;
     let test_data = vec![
-        params!["Foo", "Foo the thing.", false],
-        params!["Bar", "Apply barification.", false],
-        params!["Baz", "Bazzy.", true],
+        &[&"Foo" as &dyn ToSql, &"Foo the thing.", &false],
+        &[&"Bar", &"Apply barification.", &false],
+        &[&"Baz", &"Bazzy.", &true],
     ];
     for row in test_data {
         statement.execute(row)?;
@@ -21,16 +22,16 @@ fn populate_db_with_test_data<P: AsRef<Path> + Debug>(db_file_path: P) -> Result
 pub fn init_database<P: AsRef<Path> + Debug>(db_file_path: P) -> Result<()> {
     let conn = Connection::open(&db_file_path)?;
 
-    conn.execute("DROP TABLE IF EXISTS todo_items;", params![])?;
+    conn.execute("DROP TABLE IF EXISTS todo_items;", &[])?;
     conn.execute(
         "CREATE TABLE todo_items (
              id   INTEGER PRIMARY KEY,
              name TEXT NOT NULL,
              description TEXT,
              is_done BOOL);",
-        params![],
+        &[],
     )?;
-    
+
     populate_db_with_test_data(&db_file_path)?;
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{get, ignite, routes};
+use rocket::config::{Config, Environment, Value};
+use rocket::{get, routes};
+use rocket_contrib;
 
+use std::collections::HashMap;
 use std::env;
 use std::error;
 
 mod database;
 
+#[rocket_contrib::database("todo_db")]
+struct DbConn(rusqlite::Connection);
+
 #[get("/")]
-fn index() -> &'static str {
+fn index(conn: DbConn) -> &'static str {
     "Hello, world!"
 }
 
@@ -23,10 +29,21 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     };
 
     println!("{}", db_file_path);
-
     database::init_database(&db_file_path)?;
 
-    ignite().mount("/", routes![index]).launch();
+    let mut database_config = HashMap::new();
+    let mut databases = HashMap::new();
+    database_config.insert("url", Value::from(db_file_path));
+    databases.insert("todo_db", Value::from(database_config));
+    let config = Config::build(Environment::Development)
+        .extra("databases", databases)
+        .finalize()
+        .unwrap();
+
+    rocket::custom(config)
+        .mount("/", routes![index])
+        .attach(DbConn::fairing())
+        .launch();
 
     Ok(())
 }
